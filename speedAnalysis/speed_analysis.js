cbaf5e2bbe11c774d48fcacb6cea5939443b35c4
thopsialive <thopsisingo@gmail>
2024-04-21 14:52:11 -0400

    practice task fail

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -38,9 +38,13 @@ function endTest() {
         wpm = Math.round((typedWords / timeElapsed) * 60);
     }
     
+    // Practice Task
+    var userTypedTextLength = typedWords.length;
+
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + userTypedTextLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
