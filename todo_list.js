4e24fbbf4bc9a6f179e08d1e8c01995ab5b2048b
thopsialive <thopsisingo@gmail.com>
2024-04-22 08:59:30 -0400

    displacyTasks()

---
+++ todo_list.js
@@ -0,0 +1,28 @@
+// Step 2: Defining variables to access data
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+// Step 3: Defining various functions to access data
+function addTask() {
+    const taskText = taskInput.value.trim(); //trimming any leading or trailing whitespace
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
+
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
